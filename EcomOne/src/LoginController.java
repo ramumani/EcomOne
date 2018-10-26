fc2cc65b6a365d244f8024ae60f55fd5c6addf48
ramu.mani@hotmail.com <ramu.mani@hotmail.com>
2018-10-26 10:52:45 +0300

    Bcrypt Merge

--- EcomOne/src/LoginController.java
+++ EcomOne/src/LoginController.java
@@ -3,10 +3,6 @@ public class LoginController {
 
 	public static void main(String[] args) {
 		
-		int data[] = {1,2,3};
-		for (int i=0; i< data.length; i++) {
-			System.out.println(i);
-		}
 
 		String cryptoKey = " bCrypt";
 		int data[] = { 0, 1, 2, 3 };
