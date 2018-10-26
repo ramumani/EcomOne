f8cb45e12ba8481698109e451846ad8bb9918e27
ramu.mani@hotmail.com <ramu.mani@hotmail.com>
2018-10-26 10:45:00 +0300

    Encryption

--- EcomOne/src/LoginController.java
+++ EcomOne/src/LoginController.java
@@ -3,6 +3,11 @@ public class LoginController {
 
 	public static void main(String[] args) {
 
+		String cryptoKey = " bCrypt";
+		int data[] = { 0, 1, 2, 3 };
+		for (int i : data) {
+			System.out.println(i);
+		}
 	}
 
 }
