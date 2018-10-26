ed3eebe65a7e3d43d4f91dc8964c831e010e4fd7
dave@hotmail.com <ramu.mani@hotmail.com>
2018-10-26 10:42:22 +0300

    Audit database operations

--- EcomOne/src/LoginController.java
+++ EcomOne/src/LoginController.java
@@ -2,6 +2,11 @@
 public class LoginController {
 
 	public static void main(String[] args) {
+		
+		int data[] = {1,2,3};
+		for (int i=0; i< data.length; i++) {
+			System.out.println(i);
+		}
 
 	}
 
